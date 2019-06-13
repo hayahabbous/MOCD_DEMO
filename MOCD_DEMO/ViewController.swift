d09797c863017e8348da10be0dc182f96ca8153e
MohammedHittini <moh.hittini993@gmail.com>
2019-06-13 20:17:21 +0400

    Initial Commit

---
+++ MOCD_DEMO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MOCD_DEMO
+//
+//  Created by macbook pro on 6/13/19.
+//  Copyright © 2019 Datacellme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
